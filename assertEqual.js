c4b61bda6ddc48c7884f90705b69930aa80f4cd4
0lgaP <perronolga@gmail.com>
2022-01-10 21:55:37 +0000

    added Template Literals to assertEquals console log

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@ const happy = String.fromCodePoint(0x1F920);
 const mad = String.fromCodePoint(0x1F621);
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(happy + happy + happy + " Assertion Passed: " + actual + " === " + expected);
+    console.log(`${happy}${happy}${happy} Assertion Passed: ${actual} === ${actual}`);
   } else {
-    console.log(mad + mad + mad + " Assertion Failed: " + actual + " !== " + expected);
+    console.log(`${mad}${mad}${mad} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
