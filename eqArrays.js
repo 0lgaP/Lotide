78c4906caa4cbe9a5aa473b4f73bb3be19aade4c
0lgaP <perronolga@gmail.com>
2022-01-11 23:28:58 +0000

    added eqArray.js and added a function which compares arrays and eveluates them using the pre-built assertEqual function

---
+++ eqArrays.js
@@ -0,0 +1,35 @@
+const happy = String.fromCodePoint(0x1F920);
+const mad = String.fromCodePoint(0x1F621);
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${happy}${happy}${happy} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${mad}${mad}${mad} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1 === arr2) {
+    return true;
+  }
+  if (arr1 === null || arr2 === null) {
+    return false;
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  if (typeof arr1 !== typeof arr2) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
