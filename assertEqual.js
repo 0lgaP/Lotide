59a116e4dec94519e3bce2223fd03801e9d87d98
0lgaP <perronolga@gmail.com>
2022-01-14 00:28:53 +0000

    findKey.js added to lotide

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@ const happy = String.fromCodePoint(0x1F920);
 const mad = String.fromCodePoint(0x1F621);
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`${happy}${happy}${happy} Assertion Passed: ${actual} === ${actual}`);
+    console.log(`${happy}${happy}${happy} Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`${mad}${mad}${mad} Assertion Failed: ${actual} !== ${expected}`);
   }
