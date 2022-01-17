2f975fa16e67873de580bf49908c70ed35b49e27
0lgaP <perronolga@gmail.com>
2022-01-17 21:07:10 +0000

    changes to middle, middle test and package.json

--- Methods/middle.js
+++ Methods/middle.js
@@ -30,5 +30,4 @@ const middle = function(array) {
 
 };
 
-
 module.exports = middle;
--- Tests/middleTest.js
+++ Tests/middleTest.js
@@ -11,3 +11,14 @@ console.log(assertArrayEqual(middle([1, 2, 3, 4, 5]), [ 3 ], true)); //pass
 console.log(assertArrayEqual(middle([1, 2, 3, 4]), [ 2, 3 ], true)); //pass
 console.log(assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [ 3, 4 ], true)); //pass
 
+// 🐱 Test Cases for eqArrays
+//-----------------------------------
+
+describe("middle", () => {
+  it(`it returns an empty array if array length is 1`, () => {
+    assert.strictEqual(middle([1]), []);
+  });
+  // it(`returns false for ["1", "23"], ["1", "23", "fhqwgads"]`, () => {
+  //   assert.strictEqual(eqArrays(["1", "23"], ["1", "23", "fhqwgads"]), false);
+  // });
+});
