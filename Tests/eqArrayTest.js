fead4d92ba7e94a6545d909e6d10bae0504ad375
0lgaP <perronolga@gmail.com>
2022-01-17 14:33:12 -0500

    changes tail, assertEual and eqArrays to be slimmer

--- Tests/eqArrayTest.js
+++ Tests/eqArrayTest.js
@@ -1,14 +1,7 @@
 const assertEqual = require(`/home/olga/lighthouse/w1/lotide/Util/assertEqual`);
 const eqArrays = require(`/home/olga/lighthouse/w1/lotide/Util/eqArrays`);
 
-const assertArraysEqual = (arr1, arr2, boolian) => assertEqual(eqArrays(arr1, arr2), boolian);
-
-// 🐱 Test Cases for assertArrayEqual
+// 🐱 Test Cases for eqArrays
 //-----------------------------------
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3], true)); //pass
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"], true)); //pass
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3], false)); //pass
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "23", "3"], false)); //pass
-console.log(assertArraysEqual(["1", "3"], ["1", "23", "3"], false)); //pass
-console.log(assertArraysEqual(["1", "23", "3"], ["1", "23"], false)); //pass
-console.log(assertArraysEqual(["1", "23"], ["1", "23", "fhqwgads"], false)); //pass
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // pass
+console.log(assertEqual(eqArrays(["1", "23"], ["1", "23", "fhqwgads"]), false)); //pass
