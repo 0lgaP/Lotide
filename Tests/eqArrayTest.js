7ed8f776abe37e70e57f967551bc6e164159687f
0lgaP <perronolga@gmail.com>
2022-01-17 14:13:45 -0500

    added pass/fail comments to eqArrayTest

--- Tests/eqArrayTest.js
+++ Tests/eqArrayTest.js
@@ -4,10 +4,10 @@ const eqArrays = require(`/home/olga/lighthouse/w1/lotide/Util/eqArrays`);
 const assertArraysEqual = (arr1, arr2, boolian) => assertEqual(eqArrays(arr1, arr2), boolian);
 
 // Tests
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3], true));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"], true));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3], false));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "23", "3"], false));
-console.log(assertArraysEqual(["1", "3"], ["1", "23", "3"], false));
-console.log(assertArraysEqual(["1", "23", "3"], ["1", "23"], false));
-console.log(assertArraysEqual(["1", "23"], ["1", "23", "fhqwgads"], false));
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3], true)); //pass
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"], true)); //pass
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3], false)); //pass
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "23", "3"], false)); //pass
+console.log(assertArraysEqual(["1", "3"], ["1", "23", "3"], false)); //pass
+console.log(assertArraysEqual(["1", "23", "3"], ["1", "23"], false)); //pass
+console.log(assertArraysEqual(["1", "23"], ["1", "23", "fhqwgads"], false)); //pass
