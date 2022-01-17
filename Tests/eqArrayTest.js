b2439639702ea862f6eb543c769b173da9b082a4
0lgaP <perronolga@gmail.com>
2022-01-17 14:09:42 -0500

    created eq array test, eqArray works

---
+++ Tests/eqArrayTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require(`/home/olga/lighthouse/w1/lotide/Util/assertEqual`);
+const eqArrays = require(`/home/olga/lighthouse/w1/lotide/Util/eqArrays`);
+
+const assertArraysEqual = (arr1, arr2, boolian) => assertEqual(eqArrays(arr1, arr2), boolian);
+
+// Tests
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3], true));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"], true));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3], false));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "23", "3"], false));
+console.log(assertArraysEqual(["1", "3"], ["1", "23", "3"], false));
+console.log(assertArraysEqual(["1", "23", "3"], ["1", "23"], false));
+console.log(assertArraysEqual(["1", "23"], ["1", "23", "fhqwgads"], false));
