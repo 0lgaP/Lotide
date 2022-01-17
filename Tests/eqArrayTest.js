5b717d089fb08f5c77a6d09ab0f7691522550e3a
0lgaP <perronolga@gmail.com>
2022-01-17 21:02:31 +0000

    created cases for eqArrayTest

--- Tests/eqArrayTest.js
+++ Tests/eqArrayTest.js
@@ -1,9 +1,26 @@
 const assert = require('chai').assert;
+// list of requirements from Util
 const assertEqual = require(`../Util/assertEqual`);
 const eqArrays = require(`../Util/eqArrays`);
 
-
 // 🐱 Test Cases for eqArrays
 //-----------------------------------
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // pass
-console.log(assertEqual(eqArrays(["1", "23"], ["1", "23", "fhqwgads"]), false)); //pass
+
+describe("eqArrays", () => {
+  it("it returns true for [1, 2, 3], [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it(`returns false for ["1", "23"], ["1", "23", "fhqwgads"]`, () => {
+    assert.strictEqual(eqArrays(["1", "23"], ["1", "23", "fhqwgads"]), false);
+  });
+});
+
+describe("assertEqual", () => {
+  it(`returns assertion passed false === false`, () => {
+    assert.strictEqual(assertEqual(eqArrays(["1", "23"], ["1", "23", "fhqwgads"]), false), `🤠🤠🤠 Assertion Passed: false === false`);
+  });
+  it(`returns assertion passed true === true`, () => {
+    assert.strictEqual(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true), `🤠🤠🤠 Assertion Passed: true === true`);
+  });
+
+});
--- Tests/headTest.js
+++ Tests/headTest.js
@@ -1,14 +1,9 @@
 const assert = require('chai').assert;
-// list of requirements from Util
-const assertEqual = require('../Util/assertEqual');
 // list of requirements form Methods
 const head = require('../Methods/head');
 
-
-// 🐱 Test Cases for Head
-//---------------------------
-// console.log(assertEqual(head([ 1, 2, 3, 4, 5, 6 ]), 1)); //pass
-// console.log(assertEqual(head([ 1, 2, 3, 4, 5, 6 ]), 2)); //fail
+// 🐱 Tests for Head
+//------------------
 
 describe("head", () => {
   it("it returns 1 for [1,2,3]", () => {
